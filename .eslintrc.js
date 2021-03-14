db585a11db04a78d3645f60839207932e15d52eb
caioagiani <agianicaio@gmail.com>
2021-03-13 21:17:14 -0300

    remove: lint offense

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     indent: ['error', 2],
     'no-undef': 0,
     'no-console': 0,
+    'no-underscore-dangle': 0,
     'consistent-return': 0,
     'import/prefer-default-export': 0,
     'linebreak-style': ['error', 'unix'],
