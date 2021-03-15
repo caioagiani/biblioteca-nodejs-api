59910ec5750973dbf205757be2962036734db6b0
caioagiani <agianicaio@gmail.com>
2021-03-15 01:42:27 -0300

    fix: object values

--- src/routes/docs/index.js
+++ src/routes/docs/index.js
@@ -1,26 +1,24 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUI from 'swagger-ui-express';
 
-const options = {
+const specs = swaggerJSDoc({
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'REST API for My Diary app',
+      title: 'API - NodeJS',
       version: '1.0.0',
-      description: 'REST API for MyDiary is an online journal for personal use',
+      description: 'Sistema de reservas de livros para biblioteca.',
     },
     servers: [
       {
         url: 'http://localhost:3333/api/v1',
-        description: 'API TESTE',
+        description: 'Ambiente de desenvolvimento',
       },
     ],
   },
   apis: ['./docs/**/*.yml'],
   withCredentials: true,
-};
-
-const specs = swaggerJSDoc(options);
+});
 
 export default (docsRoute) => {
   docsRoute.use('/', swaggerUI.serve, swaggerUI.setup(specs));
