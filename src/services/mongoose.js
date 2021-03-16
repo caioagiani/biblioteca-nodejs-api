1cc343a04b594a342b2b39ec7faf82b3ff5835af
caioagiani <agianicaio@gmail.com>
2021-03-15 21:27:54 -0300

    feat: added ci tests

--- src/services/mongoose.js
+++ src/services/mongoose.js
@@ -2,14 +2,22 @@ import { connect } from 'mongoose';
 
 const { DB_USER, DB_PASS, DB_NAME, DB_HOST, NODE_ENV } = process.env;
 
-connect(`mongodb://${DB_HOST}:27017`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  user: DB_USER,
-  pass: DB_PASS,
-  dbName: NODE_ENV === 'test' ? `${NODE_ENV}_test` : DB_NAME,
-});
+const DB = NODE_ENV === 'test' ? `${NODE_ENV}_test` : DB_NAME;
+
+connect(
+  `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:27017/${DB}?authSource=admin`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+)
+  .then((result) => {
+    console.log('CONECTADO PORRA');
+  })
+  .catch((err) => {
+    console.error('DEU ERRO EM ', err);
+  });
 
 export default connect;
